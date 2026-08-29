5ffc511e675d263372e45897363bf7930df04ab8
marcoss <marcoss@MARCOS>
2026-08-29 13:38:40 -0500

    Initial tank drive controls.

--- robot1/src/main.cpp
+++ robot1/src/main.cpp
@@ -22,8 +22,6 @@ void initialize() {
 	pros::lcd::set_text(1, "Hello PROS User!");
 
 	pros::lcd::register_btn1_cb(on_center_button);
-
-	double external_ratio = 48.0 / 36.0; // 48:36 external gear ratio
 }
 
 
@@ -39,10 +37,8 @@ void autonomous() {}
 pros::Controller master(pros::E_CONTROLLER_MASTER);
 
 // temp port numbers
-pros::MotorGroup drive_fl({1, -2}, pros::v5::MotorGears::green); 
-pros::MotorGroup drive_fr({-3, 4}, pros::v5::MotorGears::green);
-pros::MotorGroup drive_bl({5, -6}, pros::v5::MotorGears::green);
-pros::MotorGroup drive_br({-7, 8}, pros::v5::MotorGears::green);
+pros::MotorGroup drive_left({1, 2, 3, 4, 5}, pros::v5::MotorGears::green); 
+pros::MotorGroup drive_right({6, 7, 8, 9, 10}, pros::v5::MotorGears::green);
 
 
 void opcontrol() {
@@ -51,22 +47,17 @@ void opcontrol() {
 
 	while (true) {
 		int forward = master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
-		int strafe = master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_X);
 		int turn = master.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X);
 
 		if (std::abs(forward) < DEADZONE) forward = 0;
-		if (std::abs(strafe) < DEADZONE) strafe = 0;
 		if (std::abs(turn) < DEADZONE) turn = 0;
 
-		int fl_power = forward + strafe + turn;
-		int bl_power = forward - strafe + turn;
-		int fr_power = forward - strafe - turn;
-		int br_power = forward + strafe - turn;
+		int left_power = forward + turn;
+		int right_power = forward - turn;
 
-		drive_fl.move(fl_power);
-		drive_bl.move(bl_power);
-		drive_fr.move(fr_power);
-		drive_br.move(br_power);
+
+		drive_left.move(left_power);
+		drive_right.move(right_power);
 
 		pros::delay(20);
 	}
